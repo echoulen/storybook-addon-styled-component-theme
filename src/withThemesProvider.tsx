787f824d5740e2f38d237fe23883e094446655de
Silvère Lestang <silvere.lestang@smyler.fr>
2019-05-03 14:20:16 +0200

    Add support for an optional CustomThemeProvider
    Close #11
    
    Usage:
    
    ```config.js
    import { ThemeProvider as MaterialThemeProvide } from '@material-ui/styles';
    import { withThemesProvider } from 'storybook-addon-styled-component-theme';
    import { ThemeProvider as StyledThemeProvider } from 'styled-components';
    
    const CustomThemeProvider = ({ theme, children }) => (
      <StyledThemeProvider theme={theme}>
        <MaterialThemeProvide theme={theme}>{children}</MaterialThemeProvide>
      </StyledThemeProvider>
    );
    
    addDecorator(withThemesProvider(themes, CustomThemeProvider));
    
    ```

--- src/withThemesProvider.tsx
+++ src/withThemesProvider.tsx
@@ -4,6 +4,6 @@ import {makeDecorator} from "@storybook/addons";
 import {ThemesProvider} from "./ThemesProvider";
 import {Theme} from "./types/Theme";
 
-export const withThemesProvider = (themes: Theme[]) => (story): JSX.Element => {
-    return <ThemesProvider themes={List(themes)}>{story()}</ThemesProvider>;
+export const withThemesProvider = (themes: Theme[], CustomThemeProvider) => (story): JSX.Element => {
+    return <ThemesProvider themes={List(themes)} CustomThemeProvider={CustomThemeProvider}>{story()}</ThemesProvider>;
 };
